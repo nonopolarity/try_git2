6e27fbcb451c1f4381b2aee11a719c946ddfe491
Kin <winterheat@gmail.com>
2011-04-14 03:23:01 -0700

    Ruby 1.9.2 Rails 3.0.6

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TryGit2::Application.config.secret_token = '391b92a70f573c31c63c41f1c9ff8a4b1347f242911fa26930085e12bd1a89f7b0ed0e832c548d2674cf961b9fe8230aec43432fc15fe92c43bf2201c7428901'
